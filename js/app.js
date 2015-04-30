5093b17746c8186cc7ad9dcbfc5217e6af62657a
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-29 20:59:33 -0400

    all content loaded

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+//ask questions 1-6 with answer options
+	$('.question-1').show();
+	$('.question-2').hide();
+	$('.question-3').hide();
+	$('.question-4').hide();
+	$('.question-5').hide();
+	$('.question-6').hide();
+
+//evaluate if correct answer is clicked
+	
+
+//show correct answer in progress bar
+
+//red out answer if incorrect
+
+//change counter in progress bar
+
+//load next question
+
+//give option to play again ?
+
+
+});
